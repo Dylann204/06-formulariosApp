a30f862a8cb34ba605919ac43225ab2832e3d159
Dylan <dylan204.play@gmail.com>
2022-03-08 16:59:57 -0500

    lo que sea

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+interface menuItem {
+  texto: string;
+  ruta: string;
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [],
+})
+export class SidemenuComponent {
+  templateMenu: menuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: './template/basicos',
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './template/dinamicos',
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches',
+    },
+  ];
+
+  reactiveMenu: menuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: './reactive/basicos',
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './reactive/dinamicos',
+    },
+    {
+      texto: 'Switches',
+      ruta: './reactive/switches',
+    },
+  ];
+}
